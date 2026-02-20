5d8d98824c59007d74fb63717d73c993a83eaa9c
Menfis-alt <geronimoorellana581@gmail.com>
2026-02-20 04:18:41 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,11 +27,10 @@ def descargar_musica(message):
     link = partes[1]
     msg = bot.reply_to(message, "⏳ Bajando audio... esto tarda un poquito.")
 
+        # Configuración de descarga
     ydl_opts = {
-         ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': 'cancion.%(ext)s',
-        # Este es el truco: usamos una fuente alternativa para evitar el bloqueo
         'force_generic_extractor': False,
         'nocheckcertificate': True,
         'postprocessors': [{
@@ -39,9 +38,9 @@ def descargar_musica(message):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        # Agregamos una identidad falsa para que YouTube no sospeche
         'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     }
+    
     
     }
 
